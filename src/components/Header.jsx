40bdffc360166e97bf2914de902cc12f4735d39d
saptarsidebnath98 <saptarsidebnath666666@gmail.com>
2024-07-15 22:38:36 +0530

    styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,16 @@ export default function Header() {
     <header>
         <nav className="bg-gradient-to-r from-blue-950 to-blue-700 text-white px-10 py-5 flex justify-between shadow-md m-5 rounded-md">
             <h3><a>Shopping Cart</a></h3>
+
+            <div className="w-1/3 rounded-r-md bg-blue-950">
             <input type="text" 
             placeholder="search products" 
-            className="w-1/3 rounded-md px-3"/>
+            className="w-4/5 rounded-md px-3 overflow-x-hidden"/>
+            <button className="w-1/5 text-xs">
+            <i className="fa-solid fa-magnifying-glass"></i>
+            </button>
+            
+            </div>
             <button>
                 <i className="fa-solid fa-cart-shopping"></i>
             </button>
