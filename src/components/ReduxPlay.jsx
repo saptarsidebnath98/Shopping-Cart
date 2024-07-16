59a24876348694167fb927366e6921309a5fdf21
saptarsidebnath98 <saptarsidebnath666666@gmail.com>
2024-07-16 22:11:55 +0530

    learned redux

--- src/components/ReduxPlay.jsx
+++ src/components/ReduxPlay.jsx
@@ -1,10 +1,10 @@
+import { useSelector, useDispatch } from 'react-redux';
 
 export default function ReduxPlay() {
+    const count = useSelector(state => state.counter.value);
   return (
-    <div className="h-screen flex bg-slate-300 justify-center items-center py-5 gap-5 font-bold">
-      <button className="h-10 w-10 rounded-md bg-slate-600">-</button>
-      <p>Counter Value 0</p>
-      <button className="h-10 w-10 rounded-md bg-slate-600">+</button>
+    <div >
+      Count value from ReduxPlay component is {count} 
     </div>
   )
 }
