8224858ab26766c2a62a7532ad9088f58db56a1b
saptarsidebnath98 <saptarsidebnath666666@gmail.com>
2024-07-16 21:26:15 +0530

    ReduxPlay.jsx created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
+import ReduxPlay from "./components/ReduxPlay";
 
 function App() {
   
   return (
     <div>
-      <Header/>
+      {/* <Header/> */}
+      <ReduxPlay/>
     </div>
   )
 }
---
+++ src/components/ReduxPlay.jsx
@@ -0,0 +1,10 @@
+
+export default function ReduxPlay() {
+  return (
+    <div className="h-screen flex bg-slate-300 justify-center items-center py-5 gap-5 font-bold">
+      <button className="h-10 w-10 rounded-md bg-slate-600">-</button>
+      <p>Counter Value 0</p>
+      <button className="h-10 w-10 rounded-md bg-slate-600">+</button>
+    </div>
+  )
+}
